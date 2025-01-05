0b5f9970ca2758c5015e9deecd43031c546416cf
Tetiana Zakharchenko <smyarga@gmail.com>
2025-01-05 19:13:31 +0200

    update

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,8 +1,40 @@
+import numpy as np
+import altair as alt
+import pandas as pd
 import streamlit as st
 
-st.header('My first app')
+st.markdown("*Streamlit* is **really** ***cool***.")
+st.header('st.write')
 
-if st.button('Say hello'):
-    st.write('Goodbye')
-else:
-    st.write('Just ordinary button')
+
+code = '''def hello():
+    print("Hello, Streamlit!")'''
+st.code(code, language="python")
+# Example 1
+
+st.write('Hello, *World!* :sunglasses:')
+
+# Example 2
+
+st.write(1234)
+
+# Example 3
+
+df = pd.DataFrame({
+     'first column': [1, 2, 3, 4],
+     'second column': [10, 20, 30, 40]
+     })
+st.write(df)
+
+# Example 4
+
+st.write('Below is a DataFrame:', df, 'Above is a dataframe.')
+
+# Example 5
+
+df2 = pd.DataFrame(
+     np.random.randn(200, 3),
+     columns=['a', 'b', 'c'])
+c = alt.Chart(df2).mark_circle().encode(
+     x='a', y='b', size='c', color='c', tooltip=['a', 'b', 'c'])
+st.write(c)
